dc5b55b16f5cd756f12c05d9f5c26458784ac9d8
vbrodar <vedran.brodar@hotmail.com>
2021-01-25 14:58:46 +0100

    Finished sidebar news and social links

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const carouselSlide = document.querySelector(".carousel-slide");
 const carouselImages = document.querySelectorAll(".carousel-slide img");
 
 //Buttons
-const prevBtn = document.querySelector("#prevBtn");
-const nextBtn = document.querySelector("#nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
+const nextBtn = document.querySelector(".nextBtn");
 
 //Counter
 let counter = 1;
