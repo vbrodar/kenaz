feafaca93aca30459b3664275ba25b56505b4a22
vbrodar <vedran.brodar@hotmail.com>
2021-01-25 09:59:36 +0100

    Minor fixes and added sidebar layout

--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,39 +1,35 @@
-const carouselImageText = document.querySelector(".carousel-image-text div");
-const carouselSlide = document.querySelector(".carousel-slide");
-const carouselImages = document.querySelectorAll(".carousel-slide img");
+"use strict";
 
-//Buttons
-const prevBtn = document.querySelector(".prevBtn");
-const nextBtn = document.querySelector(".nextBtn");
+var carouselImageText = document.querySelector(".carousel-image-text div");
+var carouselSlide = document.querySelector(".carousel-slide");
+var carouselImages = document.querySelectorAll(".carousel-slide img"); //Buttons
 
-//Counter
-let counter = 1;
-const size = carouselImages[0].clientWidth;
+var prevBtn = document.querySelector(".prevBtn");
+var nextBtn = document.querySelector(".nextBtn"); //Counter
 
-carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+var counter = 1;
+var size = carouselImages[0].clientWidth;
+carouselSlide.style.transform = "translateX(" + -size * counter + "px)"; //Button listeners
 
-//Button listeners
-
-nextBtn.addEventListener("click", () => {
+nextBtn.addEventListener("click", function () {
   if (counter >= carouselImages.length - 1) return;
   carouselSlide.style.transition = "transform 0.4s ease-in-out";
   counter++;
   carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
 });
-
-prevBtn.addEventListener("click", () => {
+prevBtn.addEventListener("click", function () {
   if (counter <= 0) return;
   carouselSlide.style.transition = "transform 0.4s ease-in-out";
   counter--;
   carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
 });
-
-carouselSlide.addEventListener("transitionend", () => {
+carouselSlide.addEventListener("transitionend", function () {
   if (carouselImages[counter].id === "lastClone") {
     carouselSlide.style.transition = "none";
     counter = carouselImageText.length - 2;
     carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
   }
+
   if (carouselImages[counter].id === "firstClone") {
     carouselSlide.style.transition = "none";
     counter = carouselImageText.length - counter;
